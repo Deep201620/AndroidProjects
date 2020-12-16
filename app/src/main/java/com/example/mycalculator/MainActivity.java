7c0e86f21221625078005f30698959d7526ac7c8
er_deep <deep19731@gmail.com>
2020-12-17 00:10:02 +0530

    First commit

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,214 @@
+package com.example.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+    Button buttonAdd,buttonSubtract,buttonMul,buttonDiv,buttonClear,buttonEqual,buttonExit;
+    String result,posi;
+    String tmp,operator;
+    int counter=-1;
+    TextView resultTextView,t1view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initControl();
+        initControlListener();
+        t1view = findViewById(R.id.textview);
+    }
+
+    private  void initControlListener(){
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("0");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("1");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("2");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("3");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("4");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("5");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("6");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("7");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("8");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("9");
+            }
+        });
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClearButtonClicked();
+            }
+        });
+        buttonSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("-");
+            }
+        });
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("+");
+            }
+        });
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("*");
+            }
+        });
+        buttonDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("/");
+            }
+        });
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEqualButtonClicked();
+            }
+        });
+        buttonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onExitButtonClicked();
+            }
+        });
+    }
+
+    private void onExitButtonClicked(){
+        Toast.makeText(MainActivity.this,"Successfully Exited from Application",Toast.LENGTH_SHORT).show();
+        finish();
+
+    }
+
+    private void onEqualButtonClicked(){
+        int res= 0;
+        try{
+            int number = Integer.valueOf(tmp);
+            int number2 = Integer.valueOf(resultTextView.getText().toString().substring(tmp.length()+operator.length(),resultTextView.length()));
+            counter = -1;
+            switch (operator) {
+                case "+":
+                    res = number + number2;
+                    break;
+
+                case "/":
+                    res = number / number2;
+                    break;
+
+                case "-":
+                    res = number - number2;
+                    break;
+
+                case "*":
+                    res = number * number2;
+                    break;
+            }
+            result = String.valueOf(res);
+            resultTextView.setText(result);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void onOperatorButtonClicked(String operator){
+        tmp = resultTextView.getText().toString();
+        //onClearButtonClicked();
+        this.operator = operator;
+        resultTextView.setText(tmp+operator);
+    }
+
+    private void onClearButtonClicked() {
+        result = "";
+        resultTextView.setText("");
+        counter=-1;
+    }
+
+    private void onNumberButtonClicked(String pos){
+        counter = counter+1;
+        result = resultTextView.getText().toString();
+        this.posi=pos;
+        result = result+posi;
+        resultTextView.setText(result);
+        System.out.println("From number function: "+counter);
+    }
+
+    private void initControl() {
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonSubtract = findViewById(R.id.buttonSub);
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonMul = findViewById(R.id.buttonMul);
+        buttonDiv = findViewById(R.id.buttonDiv);
+        buttonEqual = findViewById(R.id.buttonEqual);
+
+        resultTextView = findViewById(R.id.textview_result);
+        buttonExit = findViewById(R.id.buttonexit);
+    }
+}
